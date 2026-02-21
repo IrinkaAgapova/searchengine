0a30e286ce5c3744a28916992dda0c767a7aed76
IrinkaAgapova <irishka.agapova2@gmail.com>
2026-02-21 17:33:46 +0300

    Add files via upload

--- src/main/java/searchengine/mapping/SiteCrawler.java
+++ src/main/java/searchengine/mapping/SiteCrawler.java
@@ -1,1 +1,145 @@
+package searchengine.mapping;
 
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import searchengine.config.AppConfigProperties;
+import searchengine.model.Page;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.regex.Pattern;
+
+@Slf4j
+public class SiteCrawler extends RecursiveTask<List<Page>> {
+    private final String headUrl;
+    private final String another_url;
+    private final Set<String> visitedUrls;
+    private final AppConfigProperties connectionSetting;
+    private static final Pattern FILE_PATTERN = Pattern
+            .compile(".*\\.(webp|jpg|jpeg|png|gif|bmp|pdf|doc|docx|xls|xlsx|ppt|pptx|zip|rar|tar|gz|7z|mp3|wav|mp4|mkv|avi|mov|sql)$", Pattern.CASE_INSENSITIVE);
+    private final AtomicBoolean indexingInProgress;
+
+    public SiteCrawler(String headUrl, String url, Set<String> visitedUrls, AppConfigProperties connectionSetting, AtomicBoolean indexingInProgress) {
+        this.headUrl = headUrl;
+        this.another_url = url;
+        this.visitedUrls = visitedUrls;
+        this.connectionSetting = connectionSetting;
+        this.indexingInProgress = indexingInProgress;
+    }
+
+    public SiteCrawler(String HeadUrl, String another_url, AppConfigProperties connectionSetting, AtomicBoolean indexingInProgress) {
+        this.headUrl = HeadUrl;
+        this.another_url = another_url;
+        this.connectionSetting = connectionSetting;
+        this.visitedUrls = ConcurrentHashMap.newKeySet();
+        this.indexingInProgress = indexingInProgress;
+    }
+
+    @Override
+    public List<Page> compute() {
+        Page currentPage = new Page();
+        currentPage.setPath((another_url.substring(headUrl.length())));
+        List<Page> pages = new ArrayList<>();
+
+        if (visitedUrls.contains(another_url)) {
+            return pages;
+        }
+        if (!indexingInProgress.get()) {
+            return pages;
+        }
+        visitedUrls.add(another_url);
+
+        List<SiteCrawler> crawler = new ArrayList<>();
+
+        try {
+            fetchAndParsePage(currentPage, another_url);
+            pages.add(currentPage);
+
+            processLinks(currentPage.getContent(), visitedUrls, crawler);
+
+            collectResults(crawler, pages);
+
+        } catch (IOException e) {
+            currentPage.setCode(500);
+            currentPage.setContent(e.getMessage().isEmpty() ? "Индексация остановлена пользователем" : e.getMessage() + " url:" + another_url);
+            pages.add(currentPage);
+            if (!indexingInProgress.get()) {
+                return pages;
+            }
+        }
+        return pages;
+    }
+
+
+    private void fetchAndParsePage(Page page, String anotherUrl) throws IOException {
+
+        Connection.Response response = Jsoup.connect(anotherUrl)
+                .userAgent(connectionSetting.getUserAgent())
+                .referrer(connectionSetting.getReferer())
+                .timeout(connectionSetting.getTimeout())
+                .ignoreContentType(true)
+                .execute();
+
+        page.setCode(response.statusCode());
+        page.setContent(response.body());
+    }
+
+    private void processLinks(String content, Set<String> visitedUrls, List<SiteCrawler> crawlers) {
+        Document document = Jsoup.parse(content, another_url);
+        Elements links = document.select("a");
+        for (Element link : links) {
+            if (!indexingInProgress.get()) {
+                break;
+            }
+            String href = link.attr("abs:href").trim();
+            if (isValidLink(href)) {
+                try {
+                    SiteCrawler crawlerInstance = new SiteCrawler(headUrl, href, visitedUrls, connectionSetting, indexingInProgress);
+                    crawlerInstance.fork();
+                    crawlers.add(crawlerInstance);
+                } catch (Exception e) {
+                    log.error("Ошибка при обработке ссылки" + href + e);
+                }
+            }
+        }
+    }
+
+    public boolean isValidLink(String urls) {
+        return urls.startsWith(headUrl)
+                && !urls.contains("#")
+                && !visitedUrls.contains(urls)
+                && !FILE_PATTERN.matcher(urls).matches();
+    }
+
+    private void collectResults(List<SiteCrawler> crawlers, List<Page> pages) {
+        for (SiteCrawler crawler : crawlers) {
+            if (!indexingInProgress.get()) {
+                break;
+            }
+            pages.addAll(crawler.join());
+        }
+    }
+
+
+    public Page computePage() {
+        Page currentPage = new Page(another_url.substring(headUrl.length()));
+        try {
+            fetchAndParsePage(currentPage, another_url);
+        } catch (IOException e) {
+            log.info("Недействительный URL: {}", another_url);
+            currentPage.setCode(500);
+            currentPage.setContent(e.getMessage());
+            return currentPage;
+        }
+        return currentPage;
+    }
+}
---
+++ src/main/java/searchengine/mapping/SnippetGenerator.java
@@ -0,0 +1,92 @@
+package searchengine.mapping;
+
+import org.jsoup.Jsoup;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class SnippetGenerator {
+    private static final int CONTEXT_RADIUS = 200;
+
+    public static String generatedSnippet(String query, String content) {
+        String text = Jsoup.parse(content).text();
+        String[] words = query.split("\\s+");
+
+        int phraseIndex = findPhraseFind(text,query);
+
+        int index = phraseIndex != -1 ? phraseIndex : findFirstOccurrenceIndex(text, words);
+
+        if (index == -1) {
+            return text.length() > 100 ? text.substring(0, 300) : text;
+        }
+
+        int start = index - 50 <= 0 ? 0 : startSnippet(text,index);
+        int end = index + CONTEXT_RADIUS > text.length() ? text.length() : endSnippet(text,index);
+
+        return highlightWords(text.substring(start, end), words);
+    }
+
+    private static int findPhraseFind(String text,String phrase){
+        text = text.trim().replaceAll("\\s+", " ").toLowerCase();
+        phrase = phrase.trim().replaceAll("\\s+", " ").toLowerCase();
+        Pattern pattern = Pattern.compile(phrase);
+        Matcher matcher = pattern.matcher(text);
+        return matcher.find() ? matcher.start() : -1;
+    }
+
+    private static int findFirstOccurrenceIndex(String text, String[] words) {
+        return Arrays.stream(words).map(word-> {
+                    Pattern pattern = Pattern.compile(word,Pattern.CASE_INSENSITIVE);
+                    Matcher matcher = pattern.matcher(text);
+                    return matcher.find() && word.length() > 3 ? matcher.start() : - 1;
+                }).filter(index -> index != -1)
+                .findFirst()
+                .orElse(-1);
+    }
+    private static int startSnippet(String text,int index){
+        for(int i = index; i > 0; i--){
+            if(Character.isUpperCase(text.charAt(i)) ||
+                    text.charAt(i - 1) == '.' ||
+                    text.charAt(i - 1) == '!' ||
+                    text.charAt(i - 1) == '?')
+            {
+                return i;
+            }
+        }
+        return index;
+    }
+
+    private static int endSnippet(String text,int index){
+        for(int i = index + CONTEXT_RADIUS;i < text.length();i++){
+            if(Character.isWhitespace(text.charAt(i)) ||
+                    text.charAt(i - 1) == '.' ||
+                    text.charAt(i - 1) == '!' ||
+                    text.charAt(i - 1) == '?')
+            {
+                return i - 1;
+            }
+        }
+        return text.length();
+    }
+
+    private static String highlightWords(String text, String[] words) {
+        for (int i = 0; i < words.length; i++) {
+            words[i] = Pattern.quote(words[i]);
+        }
+        String regex = "(?<=\\PL|^)(" + String.join("|", words) + ")(?=\\PL|$)";
+
+        Pattern pattern = Pattern.compile(regex,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(text);
+
+        StringBuilder result = new StringBuilder();
+        while (matcher.find()) {
+            matcher.appendReplacement(result, "<b>" + matcher.group() + "</b>");
+        }
+        matcher.appendTail(result);
+        char lastChar = result.charAt(result.length() - 1);
+        if (lastChar != '.' && lastChar != '!' && lastChar != '?') {
+            result.append("...");
+        }
+        return result.toString();
+    }
+}
