abfa63fb3e5f7af0587f289b17b594f11eab37a2
IrinkaAgapova <irishka.agapova2@gmail.com>
2026-02-21 16:55:49 +0300

    Add files via upload

--- src/main/java/searchengine/controllers/ApiController.java
+++ src/main/java/searchengine/controllers/ApiController.java
@@ -1,1 +1,57 @@
+package searchengine.controllers;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import searchengine.dto.IndexingResponse;
+import searchengine.dto.Response;
+import searchengine.dto.ResponseSearch;
+import searchengine.dto.statistics.StatisticsResponse;
+import searchengine.services.IndexingPage;
+import searchengine.services.IndexingService;
+import searchengine.services.SearchService;
+import searchengine.services.StatisticsService;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+
+public class ApiController {
+
+    private final StatisticsService statisticsService;
+    private final IndexingService indexingService;
+    private final IndexingPage indexingPage;
+    private final SearchService searchService;
+
+
+    @GetMapping("/statistics")
+    public ResponseEntity<StatisticsResponse> statistics() {
+        return ResponseEntity.ok(statisticsService.getStatistics());
+    }
+
+    @GetMapping("/startIndexing")
+    public ResponseEntity<IndexingResponse> startIndexing() {
+        return ResponseEntity.ok(indexingService.startIndexing());
+    }
+
+    @GetMapping("/stopIndexing")
+    public ResponseEntity<IndexingResponse> stopIndexing() {
+        return ResponseEntity.ok(indexingService.stopIndexing());
+    }
+
+    @PostMapping("/indexPage")
+    public ResponseEntity<Response> indexPage(@RequestParam String url) {
+        return ResponseEntity.ok(indexingPage.indexPage(url));
+    }
+
+      @GetMapping("/search")
+       public ResponseEntity<ResponseSearch> search(@RequestParam String query,
+                                                    @RequestParam(required = false) String site,
+                                                    @RequestParam(defaultValue = "0") Integer offset,
+                                                    @RequestParam(defaultValue = "20") Integer limit){
+      return ResponseEntity.ok(searchService.systemSearch(query, site, offset, limit));
+     }
+}
