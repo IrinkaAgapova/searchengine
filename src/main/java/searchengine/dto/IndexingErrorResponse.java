06746c36fe748fdd62b7e16f282974df23c05647
IrinkaAgapova <irishka.agapova2@gmail.com>
2026-02-26 21:41:55 +0300

    Add files via upload

--- src/main/java/searchengine/dto/IndexingErrorResponse.java
+++ src/main/java/searchengine/dto/IndexingErrorResponse.java
@@ -1,8 +1,13 @@
 package searchengine.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class IndexingErrorResponse {
+    private Boolean result;
     private String error;
 }
